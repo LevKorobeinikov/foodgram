8abb38d4c47dc7f3e52dbe488dbfd2e04cf51264
LevKorobeinikov <korobeinikoffl@yandex.ru>
2024-11-27 12:13:29 +0500

    fix

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -13,7 +13,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = 'https://foooodgram.duckdns.org'
+CSRF_TRUSTED_ORIGINS = ['https://foooodgram.duckdns.org',]
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
