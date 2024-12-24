a47e502e365c56fc4fb0b4a827a3f024382beae5
LevKorobeinikov <korobeinikoffl@yandex.ru>
2024-12-24 17:27:19 +0500

    fix

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -118,6 +118,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.LimitPagination',
+    'PAGE_SIZE': 6,
 }
 
 DJOSER = {
